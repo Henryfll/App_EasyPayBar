11099f4a93127034abfa2fa94745343aefd5b9e0
FabianRBN <fabianrbn_95@hotmail.com>
2017-07-23 10:45:45 -0500

    Correccion de  cambio de orientacion y Codigo QR

--- app/src/main/java/com/example/henryf/pryeasypaybar/FragmentoPerfil.java
+++ app/src/main/java/com/example/henryf/pryeasypaybar/FragmentoPerfil.java
@@ -1,38 +1,60 @@
 package com.example.henryf.pryeasypaybar;
 
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import rm.com.longpresspopup.LongPressPopup;
+import rm.com.longpresspopup.LongPressPopupBuilder;
+import rm.com.longpresspopup.PopupInflaterListener;
+import rm.com.longpresspopup.PopupOnHoverListener;
+import rm.com.longpresspopup.PopupStateListener;
 
 /**
  * Fragmento para la pestaña "PERFIL" De la sección "Mi Cuenta"
  */
-public class FragmentoPerfil extends Fragment {
+public class FragmentoPerfil extends Fragment implements PopupInflaterListener,
+        PopupStateListener, PopupOnHoverListener, View.OnClickListener {
 
-    private TextView lblnombre;
-    private TextView lblcorreo;
-    private ImageView viewfoto;
-    private FirebaseAuth firebaseAuth;
-    private TextView lblfecha;
-    private TextView lblproducto;
-    private TextView lblcosto;
+    private static final String TAG = FragmentoPerfil.class.getSimpleName();
+    private Bitmap bitmap ;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    final FirebaseUser user = firebaseAuth.getCurrentUser();
+    private ImageView imageView;
+
+
 
     public FragmentoPerfil() {
     }
@@ -43,9 +65,13 @@ public class FragmentoPerfil extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         final FirebaseUser user = firebaseAuth.getCurrentUser();
         View fragmentoView = inflater.inflate(R.layout.fragmento_perfil, container, false);
-        lblnombre = (TextView) fragmentoView.findViewById(R.id.texto_nombre);
-        lblcorreo = (TextView) fragmentoView.findViewById(R.id.texto_email);
-        viewfoto = (ImageView) fragmentoView.findViewById(R.id.foto);
+        TextView lblnombre = (TextView) fragmentoView.findViewById(R.id.texto_nombre);
+        TextView lblcorreo = (TextView) fragmentoView.findViewById(R.id.texto_email);
+        ImageView viewfoto = (ImageView) fragmentoView.findViewById(R.id.foto);
+
+
+        // You can also chain it to the .build() mehod call above without declaring the "popup" variable before
+        // From this moment, the touch events are registered and, if long pressed, will show the given view inside the popup
 
         //System.out.println("problema: "+R.id.texto_nombre);
         lblnombre.setText(user.getDisplayName());
@@ -66,9 +92,102 @@ public class FragmentoPerfil extends Fragment {
                     .centerCrop()
                     .resize(display.getWidth()/3, display.getHeight()/5)
                     .into(viewfoto);
+        LongPressPopup popup = new LongPressPopupBuilder(fragmentoView.getContext())
+                .setTarget(viewfoto)
+                //.setPopupView(textView)// Not using this time
+                .setPopupView(R.layout.popup_layout, this)
+                .setLongPressDuration(750)
 
+                .setCancelTouchOnDragOutsideView(true)
+                .setLongPressReleaseListener(this)
+                .setOnHoverListener(this)
+                .setPopupListener(this)
+                .setTag("PopupFoo")
+                .setAnimationType(LongPressPopup.ANIMATION_TYPE_FROM_CENTER)
+                .build();
+
+        // You can also chain it to the .build() mehod call above without declaring the "popup" variable before
+        popup.register();
 
         return fragmentoView;
     }
+
+    Bitmap TextoToQr(String string)
+    {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(string, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            return  bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private class Async extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            bitmap =  TextoToQr(user.getUid());
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            imageView.setImageBitmap(bitmap);
+        }
+        @Override
+        protected void onCancelled() {
+
+        }
+
+    }
+
+    // Popup inflater listener
+    @Override
+    public void onViewInflated(@Nullable String popupTag, View root) {
+
+        imageView = (ImageView) root.findViewById(R.id.img_PerfilPopup);
+    }
+
+
+    // Touch released on a View listener
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+    // PopupStateListener
+    @Override
+    public void onPopupShow(@Nullable String popupTag) {
+       Async qrAsync = new Async();
+        qrAsync.execute();
+
+    }
+
+    @Override
+    public void onPopupDismiss(@Nullable String popupTag) {
+        //Toast.makeText(getContext(), "Popup dismissed!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // Hover state listener
+    @Override
+    public void onHoverChanged(View view, boolean isHovered) {
+        Log.e(TAG, "Hover change: " + isHovered + " on View " + view.getClass().getSimpleName());
+    }
+
+
 
 }
